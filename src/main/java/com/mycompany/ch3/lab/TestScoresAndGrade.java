f6a5ff550a45fae58b8303166e1c51c00dff630c
kjm61 <kjm61@172.16.249.53>
2019-09-16 14:06:26 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/ch3/lab/TestScoresAndGrade.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ch3.lab;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kjm61
+ */
+public class TestScoresAndGrade {
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        float testA = 0;
+        float testB = 0;
+        float testC = 0;
+        float average = 0;
+        
+        
+    }
+}
